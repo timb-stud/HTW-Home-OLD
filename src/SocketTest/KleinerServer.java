8e7eab21131706119ece44046d1ddce31a6629a6
Tobias Lana <tobias.lana@googlemail.com>
2011-02-08 14:15:12 +0100

    Server

--- src/SocketTest/KleinerServer.java
+++ src/SocketTest/KleinerServer.java
@@ -7,4 +7,27 @@ import java.io.*;
  */
 public class KleinerServer {
 
+    ServerSocket server = new ServerSocket(1234);
+
+    public KleinerServer() throws IOException{
+        while (true) {
+            Socket client = server.accept();
+            InputStream input = client.getInputStream();
+            OutputStream output = client.getOutputStream();
+            int zahl1 = input.read();
+            int zahl2 = input.read();
+            output.write(zahl1 + zahl2);
+            output.flush();
+            input.close();
+            output.close();
+        }
+    }
+    public static void main (String[] args) {
+        try {
+            KleinerServer server = KleinerServer();
+        } catch (IOException e) {
+            System.out.print(e);
+        }
+    }
+
 }
