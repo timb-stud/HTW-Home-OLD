d5020cbde84f942f3c31bdc11ae2f3d6f46eecdc
vgoekkaya <volkan.goekkaya@googlemail.com>
2011-02-08 14:59:14 +0100

    MulticastSocket test

--- src/SocketTest/Client.java
+++ src/SocketTest/Client.java
@@ -1,12 +1,7 @@
-
-
 package SocketTest;
 
-import htwhome.Main;
 import java.io.*;
 import java.net.*;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  *
@@ -14,26 +9,31 @@ import java.io.OutputStream;
  */
 public class Client {
 
-
     public Client() throws IOException {
-        Socket server = new Socket("localhost", 1234);
-        InputStream input = server.getInputStream();
-        OutputStream output = server.getOutputStream();
-        output.write(5);
-        output.write(10);
-        output.flush();
-        System.out.println(input.read());
-        server.close();
-        input.close();
-        output.close();
+
+        MulticastSocket multiSocket = new MulticastSocket(1234);
+        InetAddress group = InetAddress.getByName("255.255.255.255");
+        multiSocket.joinGroup(group);
+        String msg = "Hello";
+        DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(), group, 6789);
+        multiSocket.send(hi);
+//        Socket server = new Socket("134.96.212.18", 1234);
+//        InputStream input = server.getInputStream();
+//        OutputStream output = server.getOutputStream();
+//        output.write(5);
+//        output.write(10);
+//        output.flush();
+//        System.out.println(input.read());
+//        server.close();
+//        input.close();
+//        output.close();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        try{
+        try {
             Client client = new Client();
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
     }
