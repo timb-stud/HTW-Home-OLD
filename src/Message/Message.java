4e9831734a54fe1a65371d688ab20e63e933e321
Tobias Lana <tobias.lana@googlemail.com>
2011-02-14 14:12:02 +0100

    Konstruktor und Set/get

--- src/Message/Message.java
+++ src/Message/Message.java
@@ -10,12 +10,38 @@ package Message;
  * @author tobiaslana
  */
 public class Message {
-    private String sender = "";
+    private String sender   = "";
     private String receiver = "";
     private String mesObject = "";
 
+    public Message () {}
+    
     public Message(String sender, String receiver, String mesObject) {
-        
+        this.sender = sender;
+        this.receiver = receiver;
+        this.mesObject = mesObject;
+    }
+    
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+    public String getSender() {
+        return sender;
     }
 
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setMesObject(String mesObject) {
+        this.mesObject = mesObject;
+    }
+    public String getMesObject() {
+        return mesObject;
+    }
+
+    
 }
