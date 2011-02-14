1e6aac023ec2230911a37d364bfc031caf0a8940
vgoekkaya <volkan.goekkaya@googlemail.com>
2011-02-14 14:42:01 +0100

    Sender-Klasse geschrieben

--- src/Message/MessageSender.java
+++ src/Message/MessageSender.java
@@ -5,10 +5,22 @@
 
 package Message;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  *
  * @author tobiaslana
  */
 public class MessageSender {
 
+    public void sendMsg(Message msg) throws IOException{
+
+        Socket server = new Socket("255.255.255.255", 1234);
+        ObjectOutputStream output = new ObjectOutputStream(server.getOutputStream());
+        output.writeObject(msg);
+        server.close();
+        output.close();
+    }
 }
