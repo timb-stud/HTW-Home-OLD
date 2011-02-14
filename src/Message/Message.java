f05f33f8ef2d45390c5292f9286de661920b2117
Tobias Lana <tobias.lana@googlemail.com>
2011-02-14 13:58:01 +0100

    Messagepackage angelegt

---
+++ src/Message/Message.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Message;
+
+/**
+ *
+ * @author tobiaslana
+ */
+public class Message {
+    private String sender = "";
+    private String receiver = "";
+    private String mesObject = "";
+
+    public Message(String sender, String receiver, String mesObject) {
+        
+    }
+
+}
